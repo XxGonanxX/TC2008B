4494bb16eed9944bb44e4fcfcdfeff2293c684d6
XxGonanxX <patomorelia@hotmail.com>
2023-08-11 17:20:55 -0600

    Se termina laboratorio 1

--- Actividades_Unity/lab 1/Lab1/Assets/Scripts/MoveForward.cs
+++ Actividades_Unity/lab 1/Lab1/Assets/Scripts/MoveForward.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public int speed;
+    private bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("Movimiento", 12.0f);
+    }
+
+    private void Movimiento()
+    {
+        isMoving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMoving) {
+        // Now the object will move forward
         transform.Translate( Vector3.forward * speed * Time.deltaTime);
+        }
     }
 }
